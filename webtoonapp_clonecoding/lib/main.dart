96013792d659bffa35084da7b3621a93e67bf7dd
iloveuhyeon <pyh1111802@naver.com>
2023-12-05 23:10:54 +0900

    Nomadcoder's WebtoonApp CloneCoding start

---
+++ webtoonapp_clonecoding/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:webtoonapp_clonecoding/screens/home_screen.dart';
+import 'package:webtoonapp_clonecoding/services/api_services.dart';
+
+void main() {
+  ApiService().getTodaysToons();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: false,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+    );
+  }
+}

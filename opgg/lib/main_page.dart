ad21a0a70a71679f681506aed6782cb3d134ff93
iloveuhyeon <pyh1111802@naver.com>
2024-02-03 21:02:26 +0900

    i dont know about it

--- opgg/lib/main_page.dart
+++ opgg/lib/main_page.dart
@@ -1,20 +1,52 @@
 import 'package:flutter/material.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
   @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final TextEditingController _controller = TextEditingController();
+  String _displayText = '';
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(100.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TextField 예제'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _controller,
+              onChanged: (text) {
+                _displayText = text;
+              },
+              decoration: const InputDecoration(
+                labelText: '입력하세요',
+              ),
+            ),
+            const SizedBox(height: 20),
+            GestureDetector(
+              onTap: () => setState(() {}),
+              child: Container(
+                alignment: Alignment.center,
+                color: Colors.blue,
+                height: 40,
+                width: 80,
+                child: const Text("입력"),
+              ),
+            ),
+            Text(
+              '입력값: $_displayText',
+              style: const TextStyle(fontSize: 18),
+            ),
+          ],
         ),
       ),
     );
